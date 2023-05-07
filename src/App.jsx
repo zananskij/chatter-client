2c3aaa73b22fa46f80efd1927b5607f36e08652e
zananskij <zananskij@gmail.com>
2023-05-06 22:41:53 -0400

    updated endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import axios from "axios";
-import {UserContextProvider} from "./UserContext";
-import Routes from "./Routes";
+import axios from "axios"
+import { UserContextProvider } from "./UserContext"
+import Routes from "./Routes"
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
-  axios.defaults.withCredentials = true;
+  // axios.defaults.baseURL = "http://localhost:4040"
+  axios.defaults.baseURL = "https://new-chat-test.herokuapp.com"
+
+  axios.defaults.withCredentials = true
   return (
     <UserContextProvider>
       <Routes />
